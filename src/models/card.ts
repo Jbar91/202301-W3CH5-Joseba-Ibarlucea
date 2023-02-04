6f5092fa83d73c53f4280afb6e099a4da90c4352
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-04 11:18:13 +0100

    Update models structure

--- src/models/card.ts
+++ src/models/card.ts
@@ -1,14 +1,25 @@
-/* eslint-disable no-unused-vars */
 export type CardStructure = {
+  id: number;
   name: string;
   sprite: string;
   types: [];
   isFavorite: boolean;
+  description: string;
 };
 
 export class Card implements CardStructure {
+  public id: number;
+  public name: string;
+  public sprite: string;
+  public types: [];
   public isFavorite: boolean;
-  constructor(public name: string, public sprite: string, public types: []) {
+  public description: string;
+  constructor(dtoPoke: CardStructure) {
+    this.id = dtoPoke.id;
+    this.name = dtoPoke.name;
+    this.sprite = dtoPoke.sprite;
+    this.types = dtoPoke.types;
     this.isFavorite = false;
+    this.description = dtoPoke.description;
   }
 }
