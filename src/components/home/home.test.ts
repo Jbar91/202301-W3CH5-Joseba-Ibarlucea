201aa2602cd5c4b36ae85464e2460fb95a41d034
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-04 18:16:07 +0100

    Fix test not covering all

--- src/components/home/home.test.ts
+++ src/components/home/home.test.ts
@@ -1,18 +1,14 @@
 import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom';
+import { Component } from '../component/component';
 import { Home } from './home';
 
 describe('Given the Home Component', () => {
   describe("When it's instantiated", () => {
-    document.body.innerHTML =
-      '<slot id="app"><div class="container"></div></slot>';
-    test('Then home should be an instance of Home', () => {
-      const home = new Home('slot');
-      expect(home).toBeInstanceOf(Home);
-    });
-    test('Then it should render an h1', () => {
-      const h1 = screen.getByRole('heading');
-      expect(h1).toBeInTheDocument();
+    document.body.innerHTML = '<div class="container"></div>';
+    test('Then home should be an instance of Component', () => {
+      const home = new Home('div');
+      expect(home).toBeInstanceOf(Component);
     });
     test('Then it should render buttons', () => {
       const prevBtn = screen.getByRole('button', { name: 'Previous' });
--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -2,13 +2,12 @@
 /* eslint-disable no-unused-vars */
 import { CardsContainer } from '../cards-container/cards-container';
 import { Component } from '../component/component';
-import { Header } from '../header/header';
 
 export class Home extends Component {
   constructor(public selector: string) {
     super();
     this.template = this.createTemplate();
-    this.render('afterbegin');
+    this.render('beforeend');
   }
 
   private createTemplate() {
@@ -17,7 +16,6 @@ export class Home extends Component {
 
   render(place: globalThis.InsertPosition) {
     super.render(place);
-    new Header('#app');
     new CardsContainer('.container');
   }
 }
